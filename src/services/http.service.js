91f42b803fd5104fc61860de882fa11b8ede0e4c
agul72 <44654883+agul72@users.noreply.github.com>
2021-05-09 03:58:34 +0300

    Update http.service.js
    change uri to https

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,4 +1,4 @@
-const flights_uri = 'http://static.bidflyer.com.s3.amazonaws.com/promotional/test.json';
+const flights_uri = 'https://static.bidflyer.com.s3.amazonaws.com/promotional/test.json';
 
 export const getFlights = async () => {
     const response = await fetch(flights_uri);
